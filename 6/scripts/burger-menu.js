28ce8ce4cc2a4d88f55161dba8b4620497a39043
Keks <keks@htmlacademy.ru>
2024-01-10 14:28:28 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/scripts/burger-menu.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".main-header__toggle"),t=document.querySelector(".main-nav__wrapper");e.addEventListener("click",function(){e.classList.toggle("active"),t.classList.toggle("active")});
