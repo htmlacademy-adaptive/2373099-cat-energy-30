af7e24d586f1ac0913159997f2f8f2d453375ea4
Alexey Bulanov <masterbull0130@gmail.com>
2024-01-24 14:25:08 +0300

    Добавляет изменения js

--- source/scripts/burger-menu.js
+++ source/scripts/burger-menu.js
@@ -1,5 +1,5 @@
-const menuBtn = document.querySelector('.button-burger');
 const menu = document.querySelector('.main-nav__wrapper');
+const menuBtn = document.querySelector('.button-burger');
 
 menu.classList.remove('main-nav__wrapper--nojs');
 menuBtn.classList.remove('button-burger--nojs');
